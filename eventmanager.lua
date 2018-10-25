e41b57cba7361f1f4f28d69ab2603ec88876ae11
Emiliano Firmino <emiliano.firmino@navico.com>
2018-10-25 16:57:08 +0200

    Prevent spawn of unit if a unit is there
    Signed-off-by: Emiliano Firmino <emiliano.firmino@navico.com>

--- eventmanager.lua
+++ eventmanager.lua
@@ -12,8 +12,29 @@ function EventManager:init()
 end
 
 function EventManager:deploy()
-    for i, event in ipairs(self.eventScript) do
-       Timer.after(event.time,  function() table.insert(gameworld_officers, event.unit(event.x, event.y, event.life, event.damage, Character.LOYALTY_ENEMY)) end)
+   for i, event in ipairs(self.eventScript) do
+      Timer.after(event.time,
+         function()
+            local unit = event.unit(event.x, event.y, event.life, event.damage, Character.LOYALTY_ENEMY)
+            local overlap = false
+
+            for j, officer in ipairs(gameworld_officers) do
+               local u = vector(unit.bbox.x, unit.bbox.y)
+               local o = vector(officer.bbox.x, officer.bbox.y)
+               local d = u:dist(o)
+               overlap = d < unit.bbox.w
+
+               if overlap then
+                  break
+               end
+            end
+
+            if not overlap then
+              table.insert(gameworld_officers, unit)
+            else
+              print('skip spawn enemy because overlap')
+            end
+         end)
    end
 end
 
